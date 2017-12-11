3641e662dae44dfdf9d8675e179e403e6141c783
Felipe D Quinones <felipe.dj.quinones@gmail.com>
2017-12-10 19:26:30 -0700

    Adding functions to the program

--- Exam3/CarClass.cpp
+++ Exam3/CarClass.cpp
@@ -2,19 +2,57 @@
 #include <string>
 using namespace std;
 
-class Car {
+class Car //class Car
+{
 private:
 	int year;
 	string make;
 	int speed;
 
 public:
-	Car(int, string, int);
-		string getMake();
-		int getModel();
-		int getSpeed();
-		void accelerate();
-		void brake();
+	Car(int year, string make); 
+	string getMake();
+	int getYear();
+	int getSpeed();
+	void accelerate();
+	void brake();
 
 };
 
+
+Car::Car(int y, string model)
+{
+	year = y;
+	make = model;
+	speed = 0;
+}
+
+int Car::getYear()
+{
+	return year;
+}
+
+string Car::getMake()
+{
+	return make;
+}
+
+int Car::getSpeed()
+{
+	return speed;
+}
+
+void Car::accelerate()
+{
+	speed += 5;
+}
+
+void Car::brake()
+{
+	speed -= 5;
+}
+
+int main()
+{
+
+}
